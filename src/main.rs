a28969400b57c2b7760588b5bdd7450036287b14
Kv-Ramana01 <145438290+Kv-Ramana01@users.noreply.github.com>
2026-06-24 17:17:42 +0530

    Add initial stack VM with Push Add Print and Halt instruction

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,21 @@ mod isa;
 mod vm;
 
 use vm::VM;
+use isa::Op;
 
 fn main(){
-    let _vm = VM::new();
+    let mut vm = VM::new();
 
-    println!("MiniVM started!!");
+    let program = vec![
+        Op::Push(7),
+        Op::Push(3),
+        Op::Add,
+        Op::Print,
+        Op::Halt,
+    ];
+
+    for Instruction in program {
+        vm.execute(Instruction);
+    }
+
 }
--- src/vm.rs
+++ src/vm.rs
@@ -1,3 +1,5 @@
+use crate::isa::Op;
+
 pub struct VM {
     stack: Vec<i64>,
     globals: [i64;256],
@@ -8,6 +10,33 @@ impl VM {
         Self {
             stack: Vec::new(),
             globals: [0; 256],
+        }
+    }
+
+    pub fn execute(&mut self, op:Op) {
+        match op {
+            Op::Push(value) => {
+                self.stack.push(value);
+            }
+
+            Op::Add => {
+                let b = self.stack.pop().unwrap();
+                let a = self.stack.pop().unwrap();
+
+                self.stack.push(a+b);
+            }
+            
+            Op::Print => {
+                let value = self.stack.pop().unwrap();
+                println!("{}",value);
+            }
+
+            Op::Halt => {
+                println!("Halt");
+            }
+            _=> {
+                println!("Instruction not implemented yet");
+            }
         }
     }
 }
